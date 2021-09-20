3369f8993530aab2e9571d4d212134f1cae264dd
Bailey <bailey.gage.davis@gmail.com>
2021-09-20 19:24:15 +0900

    stories

--- home.py
+++ home.py
@@ -1,11 +1,15 @@
-from flask import Flask, request
-from flask.templating import render_template
-from stories import Story
+from flask import Flask, request, render_template
+from stories import story
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
     """Shows home page."""
-    return render_template("home.html")
 
+    return render_template("home.html", prompts=story.prompts)
+
+@app.route("/story")
+def story_page():
+    """Shows story page."""
+    return render_template("story.html", story.generate(request.args))
